82ac801ac8a17138bc5ad5ed3e65d5422c9d7d40
raptatinha <raptatinha@users-MacBook-Pro.local>
2023-05-21 14:09:43 -0700

    Add new tests

---
+++ tests/example2.spec.ts
@@ -0,0 +1,50 @@
+import { test, type Page } from '@playwright/test';
+import { HomePage } from '../pages/home-page';
+import { TopMenuPage } from '../pages/top-menu-page';
+
+const URL = 'https://playwright.dev/';
+let homePage: HomePage;
+let topMenuPage: TopMenuPage;
+const pageUrl = /.*intro/;
+
+test.beforeEach(async ({page}) => {
+  await page.goto(URL);
+  homePage = new HomePage(page);
+});
+
+async function before(page: Page) {
+  await homePage.clickGetStarted();
+  topMenuPage = new TopMenuPage(page);
+}
+
+test.describe('Playwright website', () => {
+
+  test('has title', async () => {
+    
+    await homePage.assertPageTitle();
+
+  });
+
+  test('get started link', async ({ page }) => {
+    // Act
+    await before(page);
+
+    // Assert
+    await topMenuPage.assertPageUrl(pageUrl);
+  });
+
+  test('check Java page', async ({ page }) => {
+    await test.step('Act', async () => {
+      await before(page);
+      await topMenuPage.hoverNode();
+      await topMenuPage.clickJava();
+    });
+
+    await test.step('Assert', async () => {
+      await topMenuPage.assertPageUrl(pageUrl);
+      await topMenuPage.assertNodeDescriptionNotVisible();
+      await topMenuPage.assertJavaDescriptionVisible();
+    });
+  });
+
+});
