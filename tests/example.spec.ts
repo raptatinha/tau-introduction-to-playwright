d8ea558b5bc7e38be403a1867fe4b1de9013b654
raptatinha <raptatinha@users-MacBook-Pro.local>
2023-05-21 14:25:10 -0700

    Update test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -38,7 +38,8 @@ test('check Java page', async ({ page }) => {
 
   await page.getByRole('link', {name: 'Get started'}).click();
   await page.getByRole('button', {name: 'Node.js'}).hover();
-  await page.getByRole('navigation', { name: 'Main' }).getByText('Java').click();
+  await page.getByText('Java', { exact:true }).click();
+  // await page.getByRole('navigation', { name: 'Main' }).getByText('Java').click(); // in case the locator above doesn't work, you can use this line. Remove the line above and use this one instead.
 
   await expect(page).toHaveURL('https://playwright.dev/java/docs/intro');
   await expect(page.getByText('Installing Playwright', {exact:true})).not.toBeVisible();
